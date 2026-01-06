69ba500647b64804ebf6c4704811ac34bf179d6c
RejectedSpace <npapa6306@gmail.com>
2026-01-06 17:08:56 -0500

    Created Moveset
    --Created side, down, and up attack
    --Still need jab

--- attack.gd
+++ attack.gd
@@ -1,3 +1,36 @@
 extends Node2D
 
 class_name Attack
+
+var attack_name: String
+var anim_name: String
+var startup_time: float
+var attack_time: float
+var lag_time: float
+
+@warning_ignore("shadowed_variable")
+static func create(attack_name: String, anim_name: String, startup_time: float, attack_time: float, lag_time:float) -> Attack:
+	var instance = Attack.new()
+	
+	instance.attack_name = attack_name
+	instance.anim_name = anim_name
+	instance.startup_time = startup_time
+	instance.attack_time = attack_time
+	instance.lag_time = lag_time
+	
+	return instance
+
+func get_attack_name() -> String:
+	return attack_name
+
+func get_anim_name() -> String:
+	return anim_name
+
+func get_startup_time() -> float:
+	return startup_time
+
+func get_attack_time() -> float:
+	return attack_time
+
+func get_lag_time() -> float:
+	return lag_time
--- player.gd
+++ player.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 @onready var coyote_timer = $CoyoteTimer
 @onready var glitch_timer = $GlitchTimer
 @onready var dash_timer = $DashTimer
+@onready var fall_timer = $FallTimer
 @onready var startup_timer = $StartupTimer
 @onready var attack_timer = $AttackTimer
 @onready var lag_timer = $LagTimer
@@ -15,11 +16,17 @@ const FRICTION = MAX_SPEED*3
 const AIR_RESISTANCE = FRICTION*0.3
 const MAX_JUMPS = 2
 const AIR_JUMP_OFFSET = 50
+var charge = 0
 var jumps = MAX_JUMPS
 var cooldown = false
-var attack = "dash"
+var attack: Attack
+var moveset = {
+	"dash" = Attack.create("dash", "DashAttack", 0.1, 1.25, 0.5), 
+	"side" = Attack.create("side", "SideAttack", 1, .35, 1.25),
+	"up" = Attack.create("up", "UpAttack", .75, .25, .75),
+	"down" = Attack.create("down", "DownAttack", .9, 5, 1.5)
+}
 var glitchy = false
-var attacks = []
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var dash_direction: String
 var starting_position: Vector2
@@ -30,7 +37,7 @@ func _ready() -> void:
 func _physics_process(delta: float):
 	apply_gravity(delta)
 	var direction = Input.get_axis("left", "right")
-	if !cooldown:
+	if not cooldown:
 		handle_crouch()
 		handle_jump(direction)
 		
@@ -50,9 +57,11 @@ func _physics_process(delta: float):
 		if Input.is_action_just_pressed("attack"):
 			handle_attack(direction)
 	else:
+		if Input.is_action_just_released("attack"):
+			release_attack()
 		if is_on_floor():
 			apply_friction(delta)
-			if attack == "dash" and not attack_timer.is_stopped():
+			if (attack.get_attack_name() == "dash" or attack.get_attack_name() == "down" and velocity.y >= 0) and not attack_timer.is_stopped():
 				attack_timer.stop()
 				attack_timer.timeout.emit()
 		else:
@@ -84,7 +93,13 @@ func handle_jump(direction: float) -> void:
 			jump(direction)
 
 func handle_crouch() -> void:
-	set_collision_mask_value(3, not Input.is_action_pressed("down"))
+	set_collision_mask_value(3, true)
+	if !is_on_floor():
+		set_collision_mask_value(3, not Input.is_action_pressed("down"))
+	elif Input.is_action_just_pressed("down"):
+		if not fall_timer.is_stopped():
+			set_collision_mask_value(3, false)
+		fall_timer.start()
 
 func jump(direction: float) -> void:
 	velocity.y = -JUMP_SPEED
@@ -124,38 +139,70 @@ func apply_friction(delta: float) -> void:
 	velocity.x = move_toward(velocity.x, 0, FRICTION*delta)
 
 func apply_air_resistance(delta: float) -> void:
-	var reduce = 0 if cooldown and attack == "dash" else 1 
-	velocity.x = move_toward(velocity.x, 0, AIR_RESISTANCE*reduce*delta)
+	if cooldown and attack.get_attack_name() == "dash":
+		velocity.x = move_toward(velocity.x, 0, AIR_RESISTANCE*delta)
 
 func changed_direction(direction: float) -> bool:
 	return direction * velocity.x < 0
 
 func handle_attack(direction: float) -> void:
+	var start_attack = false
 	if is_on_floor():
-		if abs(velocity.x) > MAX_SPEED*0.5:
-			cooldown = true
+		if abs(velocity.x) > MAX_SPEED*0.75:
 			velocity.y = -JUMP_SPEED*0.4
 			velocity.x = MAX_SPEED*direction*1.25
-			attack = "dash"
-			startup_timer.start(0.1)
+			attack = moveset.get("dash")
+			start_attack = true
+		
 		elif direction:
-			cooldown = true
-			attack = "left_smash"
-			startup_timer.start(0.5)
+			attack = moveset.get("side")
+			start_attack = true
+		
+		elif Input.is_action_pressed("up") and not Input.is_action_pressed("down"):
+			attack = moveset.get("up")
+			start_attack = true
+	
+	if not start_attack and Input.is_action_pressed("down") and not Input.is_action_pressed("up"):
+		attack = moveset.get("down")
+		start_attack = true
+	
+	if start_attack:
+		cooldown = true
+		charge = 1
+		if attack.get_attack_name() == "down" and not is_on_floor():
+			startup_timer.timeout.emit()
+		else:
+			startup_timer.start(attack.get_startup_time())
+
+func release_attack() -> void:
+	if not startup_timer.is_stopped():
+		charge = (startup_timer.get_wait_time() - startup_timer.get_time_left())/attack.get_startup_time()
+		
+		startup_timer.stop()
+		startup_timer.timeout.emit()
 
 func apply_animations(direction: float) -> void:
 	if cooldown:
-		match attack:
-			"dash":
-				animation.play("DashAttack")
-			"left_smash":
-				animation.play("LeftSmash")
+		animation.set_animation(attack.get_anim_name())
+		
+		if not startup_timer.is_stopped():
+			animation.set_frame(0)
+		elif not attack_timer.is_stopped():
+			animation.set_frame(1)
+		else:
+			animation.set_frame(2)
+		
 	else:
 		if direction:
 			animation.flip_h = direction > 0
 			animation.offset.x = 32 if direction > 0 else 0
-			animation.play("Walk")
-			animation.speed_scale = velocity.x/MAX_SPEED*5
+			
+			if abs(velocity.x) > MAX_SPEED*0.75:
+				animation.play("Run")
+				animation.speed_scale = 1
+			else:
+				animation.play("Walk")
+				animation.speed_scale = abs(velocity.x)/MAX_SPEED*5
 		else:
 			animation.play("Idle")
 			animation.speed_scale = 1
@@ -181,18 +228,19 @@ func _on_glitch_timer_timeout() -> void:
 	glitchy = false
 
 func _on_startup_timer_timeout() -> void:
-	match attack:
-		"dash":
-			$DashAttack.visible = true
-			attack_timer.start(1.25)
+	if attack.get_attack_name() == "down":
+		if is_on_floor():
+			velocity.y = -JUMP_SPEED * (log(60*charge)/(2*log(60)) + 0.5)
+		else:
+			velocity.y = abs(velocity.y) * 1.5
+	attack_timer.start(attack.get_attack_time())
 
 func _on_attack_timer_timeout() -> void:
-	match attack:
-		"dash":
-			$DashAttack.visible = false
-			lag_timer.start(0.5)
+	var lag_proportion = log(60*charge)/(2*log(60)) + 0.5
+	
+	lag_timer.start(attack.get_lag_time()*lag_proportion)
 
 func _on_lag_timer_timeout() -> void:
 	cooldown = false
-	
+
 signal air_jump
